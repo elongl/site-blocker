f853a95ec5a8d0f0030d8fb01ba93379343f793c
Elon Gliksberg <elongliks@gmail.com>
2022-12-26 23:12:47 +0200

    Using storage.

--- src/content.js
+++ src/content.js
@@ -1,4 +1,12 @@
-if (location.host === 'twitter.com') {
-  const blockedPageURL = chrome.runtime.getURL('static/blocked.html');
+const blockSite = async (host) => {
+  const blockedSites = (await chrome.storage.sync.get("blockedSites")) || [];
+  await chrome.storage.sync.set({
+    blockedSites: [...blockedSites, host],
+  });
+  console.log(`Successfully added "${host}" to blocked sites.`);
+};
+
+if (blockedSites.includes(location.host)) {
+  const blockedPageURL = chrome.runtime.getURL("static/blocked.html");
   window.location.href = blockedPageURL;
 }
